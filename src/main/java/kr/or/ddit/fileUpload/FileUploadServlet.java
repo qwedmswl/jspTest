5f69ffdd53f2b8cd0b4ceeba7375103feb30f19e
dy <qwedmswl@gmail.com>
2020-10-21 09:31:41 +0900

    fileUpload

---
+++ src/main/java/kr/or/ddit/fileUpload/FileUploadServlet.java
@@ -0,0 +1,26 @@
+package kr.or.ddit.fileUpload;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/FileUpload")
+public class FileUploadServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public FileUploadServlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/fileUpload/uploadView.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
---
+++ src/main/webapp/fileUpload/uploadView.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	userId : <input type="text" name="userid" value="brown"/> <br>
+	file : <input type="file" name="img" /> <!-- 값을 미리 설정하는게 불가능 (보안문제) -->
+	<button type="submit">전송</button>
+</body>
+</html>
