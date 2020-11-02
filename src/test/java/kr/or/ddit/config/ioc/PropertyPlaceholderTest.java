9fdd0152165a1ba00259c40539090e94b4f69c29
dy <qwedmswl@gmail.com>
2020-11-02 16:07:14 +0900

    실습  . property-placeholder

---
+++ src/test/java/kr/or/ddit/config/ioc/PropertyPlaceholderTest.java
@@ -0,0 +1,33 @@
+package kr.or.ddit.config.ioc;
+
+import static org.junit.Assert.*;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import kr.or.ddit.ioc.DbProperty;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:kr/or/ddit/config/spring/ioc/property-placeholder.xml"})
+public class PropertyPlaceholderTest {
+
+	@Resource(name="dbProperty")
+	private DbProperty dbproperty;
+	
+	@Test
+	public void test() {
+		/***Given***/
+		
+
+		/***When***/
+
+		/***Then***/
+		assertEquals("DY", dbproperty.getUser());
+		assertEquals("java", dbproperty.getPass());
+	}
+
+}
